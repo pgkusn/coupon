c9207485132762301959ac7c881e03e1d57b71b1
kenge <pgkusn@gmail.com>
2020-04-27 23:25:24 +0800

    update

--- assets/resource/components/couponCounter/detail.vue
+++ assets/resource/components/couponCounter/detail.vue
@@ -5,7 +5,22 @@
             <h2 class="subtitle">Vidol 精彩戲劇．電影看不完</h2>
             <h1 class="title">優惠兌換櫃台</h1>
             <form action="" method="post">
-                <input type="text" name="">
+                <div class="field">
+                    <input type="text" name="name" required v-model="info.name" :class="{ active: info.name.length }">
+                    <div class="text">訂閱者姓名</div>
+                </div>
+                <div class="field">
+                    <input type="text" name="email" required v-model="info.email" :class="{ active: info.email.length }">
+                    <div class="text">連絡信箱</div>
+                </div>
+                <div class="field">
+                    <input type="text" name="phone" required v-model="info.phone" :class="{ active: info.phone.length }">
+                    <div class="text">聯絡電話</div>
+                </div>
+                <div class="field">
+                    <input type="text" name="address" required v-model="info.address" :class="{ active: info.address.length }">
+                    <div class="text">地址</div>
+                </div>
             </form>
         </div>
     </div>
@@ -14,5 +29,15 @@
 <script>
 module.exports = {
     name: 'Detail',
+    data() {
+        return {
+            info: {
+                name: '',
+                email: '',
+                phone: '',
+                address: '',
+            }
+        }
+    },
 }
 </script>
